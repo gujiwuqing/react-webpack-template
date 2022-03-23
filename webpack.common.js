ccec8e2c2e3771096ed87b867a0890efd04a61a3
孤寂无情 <15279279713@163.com>
2022-03-23 21:53:20 +0800

    feat: 线上环境设置base路径

--- webpack.common.js
+++ webpack.common.js
@@ -2,82 +2,82 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ProgressPlugin = require('progress-webpack-plugin');
 module.exports = {
-    entry: './src/main.tsx', // 入口文件
-    output: {
-        path: path.resolve(__dirname, 'dist'), // 输出目录
-        filename: 'bundle.js', // 输出文件
-        clean: true,
-    },
-    devServer: {
-        static: './dist',
-        open: true,
-        port: 10099
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/, // ts文件处理
-                exclude: /(node_modules|bower_components)/,
-                use: ['ts-loader', {
-                    loader: 'babel-loader',
-                    options: {
-                        "plugins": [
-                            [
-                                "import", {
-                                "libraryName": "antd",
-                                "style": true
-                            }
-                            ]
-                        ],
-                        presets: [
-                            // 添加 preset-react
-                            require.resolve('@babel/preset-react'),
-                            [require.resolve('@babel/preset-env'), {modules: false}]
-                        ],
-                    }
-                }
-                ]
-            },
-            {
-                test: /\.less$/i,
-                use: ['style-loader', 'css-loader', {
-                    loader: "less-loader",
-                    options: {
-                        lessOptions: {
-                            javascriptEnabled: true,
-                        },
-                    },
-                },]
-            },
-            {
-                test: /\.css$/, // css文件处理
-                use: ['style-loader', 'css-loader']
-            }
+  entry: './src/main.tsx', // 入口文件
+  output: {
+    path: path.resolve(__dirname, 'dist'), // 输出目录
+    filename: 'bundle.js', // 输出文件
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+    port: 10099
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // ts文件处理
+        exclude: /(node_modules|bower_components)/,
+        use: ['ts-loader', {
+          loader: 'babel-loader',
+          options: {
+            "plugins": [
+              [
+                "import", {
+                "libraryName": "antd",
+                "style": true
+              }
+              ]
+            ],
+            presets: [
+              // 添加 preset-react
+              require.resolve('@babel/preset-react'),
+              [require.resolve('@babel/preset-env'), {modules: false}]
+            ],
+          }
+        }
         ]
+      },
+      {
+        test: /\.less$/i,
+        use: ['style-loader', 'css-loader', {
+          loader: "less-loader",
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+            },
+          },
+        },]
+      },
+      {
+        test: /\.css$/, // css文件处理
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // 模板路径
+    }),
+    new ProgressPlugin({
+      activeModules: false,
+      entries: true,
+      handler(percentage, message, ...args) {
+        // custom logic
+      },
+      modules: true,
+      modulesCount: 5000,
+      profile: false,
+      dependencies: true,
+      dependenciesCount: 10000,
+      percentBy: null,
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html' // 模板路径
-        }),
-      new ProgressPlugin({
-        activeModules: false,
-        entries: true,
-        handler(percentage, message, ...args) {
-          // custom logic
-        },
-        modules: true,
-        modulesCount: 5000,
-        profile: false,
-        dependencies: true,
-        dependenciesCount: 10000,
-        percentBy: null,
-      })
-],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@components': path.resolve(__dirname, 'src/components'),
-        },
-        extensions: ['.ts', '.tsx', '.js', '.json'] // 添加ts和tsx后缀
-    },
+    extensions: ['.ts', '.tsx', '.js', '.json'] // 添加ts和tsx后缀
+  },
 };
